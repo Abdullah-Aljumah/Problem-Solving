32c8ab1ee8e909dbba3f4ebae8a765dcbbbb10a1
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-11-10 11:55:48 +0300

    re add problem solving

---
+++ problemSolving.js
@@ -0,0 +1,18 @@
+// function isPalindrome(x) {
+//   let reverse = x.toLowerCase().split("").reverse().join();
+//   x = x.toLowerCase().split("").join();
+//   for (i = 0; i < x.length; i++) {
+//     if (x[i] == reverse[i]) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+// isPalindrome();
+
+function isPalindrome(x) {
+  let reverse = x.toLowerCase().split("").reverse().join() == x.toLowerCase().split("") ? true:false;
+}
+
+isPalindrome();
