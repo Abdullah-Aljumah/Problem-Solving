76f21362d66064fb798b5567f2c265b769cdde22
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-11-16 11:27:07 +0300

    leap years

--- leapUears.js
+++ leapUears.js
@@ -5,9 +5,7 @@ function isLeapYear(year) {
       else if (year % 4 == 0 && year % 100 != 0 ) {
         return true
       }
-    else if (year % 400 !== 0 ) {
-      return false
-    } else {
+   else {
       return false
     }
   }
