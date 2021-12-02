094af634ca24f32df4b044584e429006bbec8975
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-02 16:30:48 +0300

    add higher order

---
+++ higherOrder.js
@@ -0,0 +1,13 @@
+function findSenior(list) {
+  let max = 0;
+
+  for (i = 0; i < list.length; i++) {
+    if (list[i].age > max) {
+      max = list[i].age;
+    }
+  }
+  let answer = list.filter((item) => {
+    return item.age === max;
+  });
+  return answer;
+}
